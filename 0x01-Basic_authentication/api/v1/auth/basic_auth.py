8b0c6a777c179096f7a4741ef97b7bd93ccfa998
ila36IX <jbariali002@gmail.com>
2024-09-19 03:57:40 +0100

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,7 +41,8 @@ class BasicAuth(Auth):
             return None, None
         if ":" not in decoded_base64_authorization_header:
             return None, None
-        return tuple(decoded_base64_authorization_header.split(':'))
+        print(tuple(decoded_base64_authorization_header.split(':', 1)))
+        return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(self,
                                      user_email: str,

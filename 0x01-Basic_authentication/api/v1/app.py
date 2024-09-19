73a0c922a29f5948df4a648cb73636438d33c34b
ila36IX <jbariali002@gmail.com>
2024-09-19 01:59:30 +0100

    Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#/usr/bin/env python3
 """
 Route module for the API
 """
@@ -12,7 +12,27 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if getenv("AUTH_TYPE", None):
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
+
+@app.before_request
+def check_protected():
+    if auth is None:
+        return
+    protected_urls = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ] 
+    if auth.require_auth(request.path, protected_urls) is False:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -26,6 +46,11 @@ def not_auth(error) -> str:
     """Unauthorized resourse"""
     return jsonify({"error": "Unauthorized"}), 401
 
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """forbidden resourse"""
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+
+Manage the API authentication.
+
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Manage the API authentication."""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Return False if path doesn't requare auth, and True if the path
+        is protected
+            @excluded_paths: The paths that requered auth
+        """
+        if path is not None and len(path) > 1:
+            if path[-1] != "/":
+                path += "/"
+        if path is None or excluded_paths is None:
+            return True
+        if path not in excluded_paths:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Return the credintiols used by the user to authonticate"""
+        if request is None:
+            return None
+        return request.args.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Docs will ve added later after understanding what the function do"""
+        return None

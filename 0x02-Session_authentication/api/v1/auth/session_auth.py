d4cec267e7d1df57e57d8d0ed162e4101baa7f31
ila36IX <jbariali002@gmail.com>
2024-09-20 02:53:41 +0100

    implementing Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,13 +3,8 @@
 Implimenting the Session authontication
 """
 from api.v1.auth.auth import Auth
-import base64
-from flask import request, jsonify, make_response
-from typing import TypeVar
 from uuid import uuid4
 from models.user import User
-from api.v1.views import app_views
-from os import getenv
 
 
 class SessionAuth(Auth):
@@ -34,29 +29,5 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None):
         """Returns a User instance based on a cookie value"""
-        print(self.user_id_by_session_id)
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
-
-
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login():
-    """Hondle login using email and password"""
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if email is None or len(email) == 0:
-        return jsonify({"error": "email missing"}), 400
-    if password is None or len(password) == 0:
-        return jsonify({"error": "password missing"}), 400
-    users = User.search({"email": email})
-    if len(users) == 0:
-        return jsonify({"error": "no user found for this email"}), 404
-    user = users[0]
-    if user.is_valid_password(password):
-        from api.v1.app import auth
-        new_session = auth.create_session(user.id)
-        response = make_response(user.to_json())
-        cookie_name = getenv("SESSION_NAME")
-        response.set_cookie(cookie_name, new_session)
-        return response
-    return jsonify({"error": "wrong password"}), 401
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,32 @@
+
+#!/usr/bin/env python3
+"""
+Implimenting the Session authontication routes
+"""
+from flask import request, jsonify, make_response
+from models.user import User
+from api.v1.views import app_views
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """Hondle login using email and password"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None or len(email) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if password is None or len(password) == 0:
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({"email": email})
+    if len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = users[0]
+    if user.is_valid_password(password):
+        from api.v1.app import auth
+        new_session = auth.create_session(user.id)
+        response = make_response(user.to_json())
+        cookie_name = getenv("SESSION_NAME")
+        response.set_cookie(cookie_name, new_session)
+        return response
+    return jsonify({"error": "wrong password"}), 401

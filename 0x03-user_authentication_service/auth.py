a7eeecc5b3e1a7f6ca7a04c2ec3ead7d9050a6ae
ila36IX <jbariali002@gmail.com>
2024-09-27 00:58:00 +0100

    implement my own authentication system, BOOM

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,14 +49,13 @@ class Auth:
         except NoResultFound:
             return False
 
-
     def create_session(self, email: str) -> Optional[str]:
         """Find the user with the email, generate a new UUID and store it in
         the database as the user’s session_id
         """
         try:
             user = self._db.find_user_by(email=email)
-            session_id = self._generate_uuid()
+            session_id = _generate_uuid()
             self._db.update_user(user_id=user.id, session_id=session_id)
             return session_id
         except NoResultFound:
@@ -78,17 +77,13 @@ class Auth:
         except NoResultFound:
             return
 
-    def _generate_uuid() -> str:
-        """return a string representation of a new UUID"""
-        return str(uuid4())
-
     def get_reset_password_token(self, email: str) -> str:
         """Generate a UUID and update the user’s reset_token database field"""
         if type(email) is not str or len(email) == 0:
             raise ValueError
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = self._generate_uuid()
+            reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:

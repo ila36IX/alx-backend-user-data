290bd6899001f88c9a6e876e6e153088854923fd
ila36IX <jbariali002@gmail.com>
2024-09-26 23:38:47 +0100

    implement my own authentication system, BOOM

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 SQLAlchemy model named User for a database table named users
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -13,8 +13,9 @@ class User(Base):
     Model to interact with the users table
     """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)

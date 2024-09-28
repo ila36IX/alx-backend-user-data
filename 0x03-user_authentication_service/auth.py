9c961812007dfb69492365809e8e8f061052c75b
ila36IX <jbariali002@gmail.com>
2024-09-28 02:50:34 +0100

    implement my own authentication system, BOOM

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -98,10 +98,8 @@ class Auth:
             raise ValueError
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            user.hashed_password = _hash_password(password)
-            user.reset_token = None
-            # self._db.update_user(user.id,
-            #                      hashed_password=_hash_password(password),
-            #                      reset_token=None)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
